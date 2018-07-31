acd1125e3a623fd774869a2a084a65539698c130
Wes Baker <wes@wesbaker.com>
2018-07-31 13:15:32 -0400

    Initial commit of Stardew Valley Wiki Alfred workflow.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Alfred Stardew Valley Wiki Workflow
+
+Search Stardew Valley Wiki from the comfort of Alfred.
+
+![Workflow in action](media/example.png)
+
+## Install
+
+```sh
+npm install --global alfred-stardewvalleywiki
+```
+
+*Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+## Usage
+
+In Alfred, type `svw` then your query to search for Stardew Valley Wiki items.
+Hit <kbd>Enter</kbd> to go to the items's Stardew Valley Wiki page.
+
+## License
+
+MIT © [Wes Baker](http://wesbaker.com)
---
+++ index.js
@@ -0,0 +1,32 @@
+const alfy = require("alfy");
+
+alfy
+  .fetch("https://stardewvalleywiki.com/mediawiki/api.php", {
+    query: {
+      action: "opensearch",
+      format: "json",
+      search: alfy.input,
+      namespace: 0,
+      limit: 10
+    }
+  })
+  .then(([query, words, _, urls]) => {
+    if (words.length === 0 && urls.length === 0) {
+      const encodedInput = encodeURIComponent(alfy.input);
+      return alfy.output([
+        {
+          title: `No results found for '${alfy.input}'`,
+          subtitle: `Search Stardew Valley Wiki for '${alfy.input}'`,
+          arg: `https://stardewvalleywiki.com/mediawiki/index.php?search=${encodedInput}`
+        }
+      ]);
+    }
+
+    const results = words.map((word, index) => {
+      return {
+        title: word,
+        arg: urls[index]
+      };
+    });
+    alfy.output(results);
+  });
